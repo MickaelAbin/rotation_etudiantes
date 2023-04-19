3e6ce67ed86692af59f852664a3655d20b6bfdb9
Jonas Oger <jonas.oger@oniris-nantes.fr>
2023-04-19 10:05:36 +0200

    Création de l'entité Enrolment

---
+++ src/Entity/Enrolment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EnrolmentRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EnrolmentRepository::class)
+ * @ORM\Table(name = "enrolments")
+ */
+class Enrolment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy = "AUTO")
+     * @ORM\Column(name = "id", type = "integer", options = {"unsigned": true})
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(name = "date", type = "date", nullable = false)
+     */
+    private ?DateTimeImmutable $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function setDate(DateTimeImmutable $date): self
+    {
+        $this->date = $date;
+        return $this;
+    }
+}
---
+++ src/Repository/EnrolmentRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Enrolment;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Enrolment>
+ *
+ * @method Enrolment|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Enrolment|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Enrolment[]    findAll()
+ * @method Enrolment[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EnrolmentRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Enrolment::class);
+    }
+
+    public function add(Enrolment $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Enrolment $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Enrolment[] Returns an array of Enrolment objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Enrolment
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
