1a1f605de5e059e2b7d006a3533128293803ce1d
Jonas Oger <jonas.oger@oniris-nantes.fr>
2023-04-21 09:33:21 +0200

    Modification de la base twig (bootstrap, font awesome, favicon) bis (correction d'un import)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use Symfony\Bridge\Doctrine\ManagerRegistry;
+
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
