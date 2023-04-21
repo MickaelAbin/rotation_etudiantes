99d60b03bd1203a20f437898962d329fef13dcd5
Jonas Oger <jonas.oger@oniris-nantes.fr>
2023-04-21 09:26:27 +0200

    Modification de la base twig (bootstrap, font awesome, favicon)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,34 @@
 
 namespace App\Controller;
 
-use App\Repository\StudentRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private ManagerRegistry $managerRegistry;
+    public function __construct(ManagerRegistry $managerRegistry) {
+        $this->managerRegistry = $managerRegistry;
+    }
+
     /**
-     * @Route("/home", name="app_home")
+     * @Route(path = "/", name = "home")
      */
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('test/index.html.twig', [
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route(path = "/moodle", name = "moodle")
+     */
+    public function test(): Response
+    {
+        $connection = $this->managerRegistry->getConnection('mdl_user');
     }
 
 }
