5623a28501e25e394106013a3dc02f3818ccf54d
Jonas Oger <jonas.oger@oniris-nantes.fr>
2023-04-24 09:49:19 +0200

    Ajout du bloc twig main dans la base twig

---
+++ public/js/base.js
@@ -0,0 +1,6 @@
+    var navTop = document.getElementById(navbar-top);
+    var
+
+    navTop.getElementsByClassName('nav-link').addEventListener('click', function (event) {
+        navTop.children.forEach
+    })
